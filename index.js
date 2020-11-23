349d6869a91aabbc092e83e6db998511da5d4ccf
Michael Shuff <mjshuff23@gmail.com>
2020-11-23 01:28:53 -0500

    added chalk

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
+const chalk = require('chalk');
+
 
 const client = new Discord.Client();
+const greenBright = chalk.greenBright;
+const redBright = chalk.redBright;
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log(greenBright(`${redBright('Deadpool')} is up and running!`));
+});
+
+client.on('message', message => {
+    console.log(message.content);
 });
 
 client.login('NzgwMzA5NTgyMzkxOTM1MDA2.X7tN7Q.No7ObcgR3OxNIubqtI2rwuHJJyI');
