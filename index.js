8dda43159674d42f769e765efee152efc8dbb868
Michael Shuff <mjshuff23@gmail.com>
2020-11-23 01:12:36 -0500

    updated ESLint rules and added index.js, bot is running

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('NzgwMzA5NTgyMzkxOTM1MDA2.X7tN7Q.No7ObcgR3OxNIubqtI2rwuHJJyI');
