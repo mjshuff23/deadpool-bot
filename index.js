e6409bb498f67a01d7d8b99d7bddb0dd512f1652
Michael Shuff <mjshuff23@gmail.com>
2020-11-26 05:15:49 -0500

    basic setup finished

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const Discord = require('discord.js');
 const chalk = require('chalk');
+const { prefix, token } = require('./config.json');
 
-
-const client = new Discord.Client();
 const greenBright = chalk.greenBright;
 const redBright = chalk.redBright;
+const whiteBright = chalk.whiteBright;
+const inverse = chalk.inverse;
+
+const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log(greenBright(`${redBright('Deadpool')} is up and running!`));
 });
 
 client.on('message', message => {
-    console.log(message.content);
+    if (!message.content.startsWith(prefix)) {
+        if (message.author.bot) return;
+
+        return console.log(`${redBright(message.author.username)}: ${whiteBright(message.content)}`);
+    }
+
+
+    if (message.content === '!ping') {
+        message.channel.send('I just shit in your kitty litter... ^_^');
+    }
 });
 
-client.login('NzgwMzA5NTgyMzkxOTM1MDA2.X7tN7Q.No7ObcgR3OxNIubqtI2rwuHJJyI');
+client.login(token);
